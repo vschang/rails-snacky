807c8f5ab9b7bde05b396a185b781591ce8da755
Valerie Chang <valeriechang0@gmail.com>
2022-10-12 12:38:52 +0200

    edit form is multistep

---
+++ app/javascript/controllers/multistep_form_2_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="multistep-form-2"
+export default class extends Controller {
+  static targets = [ "step" ]
+  static values = {index: Number, page: String};
+
+  connect() {
+    console.log('form controller edit connected')
+  }
+
+  next = () => {
+    // const stepsNext = document.getElementsByClassName('post-step-next')
+    if (this.indexValue > 2){
+      this.indexValue === 3
+    } else {
+      this.indexValue++
+    }
+    // console.log(this.indexValue)
+  }
+
+  back = () => {
+    // const stepsBack = document.getElementsByClassName('post-step-back')
+    // console.log(stepsBack)
+    if (this.indexValue < 1){
+      this.indexValue === 0
+    } else {
+      this.indexValue--
+    }
+    // console.log(this.indexValue)
+  }
+
+  indexValueChanged() {
+    this.showCurrentStep()
+  };
+
+  showCurrentStep() {
+    this.stepTargets.forEach((element, index) => (
+      element.hidden = index != this.indexValue
+    ))
+  }
+}
